a1b677f565ed1a4af2d1cac7e90768a55e0eb103
xhanalexander <xhanalexander@gmail.com>
2022-06-23 03:10:22 +0700

    update

--- admin/create.php
+++ admin/create.php
@@ -5,25 +5,35 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Add new Book</title>
       <script src="https://cdn.tailwindcss.com"></script>
+      <!-- <script src="https://unpkg.com/flowbite@1.4.7/dist/datepicker.js"></script> -->
    </head>
-<body>
-   <h1 class="text-3xl font-semibold">New book</h1>
-
-   <section class="">
-      <form action="book.php" method="post" name="create_form" autocomplete="on" >
+<body class="">
+   
+   <section class="flex flex-row justify-center h-screen">
+      <form action="book.php" method="post" name="create_form" autocomplete="on" class="my-auto" >
          <main>
-            <div>
-               <h3>add new guest room</h3>
+            <div class="mb-8">
+               <h1 class="text-3xl font-semibold text-center">New book</h1>
+               <h3 class="text-xl text-center">add new guest room</h3>
             </div>
-
-            <header>
-               <input type="text" name="nama" placeholder="John" maxlength="15" minlength="3" required>
-               <input type="date" name="datang" required>
-               <input type="date" name="pulang" required>
+            
+            <label for="nama" class="font-semibold text-lg">Guest Name :</label>
+            <header class="mb-4">
+               <input type="text" name="nama" placeholder="John Appleseed" 
+                  class="border p-2 w-full border-slate-300 bg-gray-100 rounded" 
+                  maxlength="15" minlength="3" required>
             </header>
 
-            <header>
-               <select name="tipe" required>
+            <label for="date" class="font-semibold text-lg">Date Book :</label>
+            <header class="mb-4">
+               <input type="date" name="datang" class="border p-2 bg-gray-100 border-slate-300 rounded" required >
+               <span class="mx-4">To</span>
+               <input type="date" name="pulang" class="border p-2 bg-gray-100 border-slate-300 rounded" required>
+            </header>
+            
+            <label for="date" class="font-semibold text-lg">Room Type :</label>
+            <header class="mb-4">
+               <select name="tipe" class="border w-full p-2 bg-gray-100 border-slate-200 rounded" required>
                   <option value="Regular Room">Regular Room</option>
                   <option value="Senior Room">Senior Room</option>
                   <option value="Deluxe Room">Deluxe Room</option>
@@ -32,20 +42,19 @@
                   <option value="Kings Room">Kings Room</option>
                </select>
             </header>
-
             
-            <header>
-               <label for="jumlah">Jumlah</label>
-               <input type="number" name="jumlah" required>
+            <label for="date" class="font-semibold text-lg">Guest People :</label>
+            <header class="mb-4">
+               <input type="number" name="jumlah" class="border p-2 bg-gray-100 w-full border-slate-300 rounded" placeholder="1 Guest" required>
             </header>
             
-            <header>
-               <label for="harga">Harga</label>
-               <input type="number" name="harga" placeholder="Rp1.000.000" maxlength="15" minlength="3" required>
+            <label for="date" class="font-semibold text-lg">Price :</label>
+            <header class="mb-4">
+               <input type="number" name="harga" placeholder="Rp1.000.000" class="border p-2 bg-gray-100 w-full border-slate-300 rounded" maxlength="15" minlength="3" required>
             </header>
 
             <header>
-               <input type="submit" name="booking" value="book">
+               <input type="submit" class="bg-purple-500 hover:bg-purple-600 text-white p-2 rounded w-32 cursor-pointer rounded text-lg" name="booking" value="Create">
             </header>
          </main>
       </form>
